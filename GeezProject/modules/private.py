f1fdf1bdcac3e7389f7401f2398dfc17aae6227a
ikymaniez <84445414+ikymaniez@users.noreply.github.com>
2021-05-21 08:28:29 +0700

    Create private.py

--- GeezProject/modules/private.py
+++ GeezProject/modules/private.py
@@ -10,20 +10,34 @@ def _start(client, message):
     client.send_message(message.chat.id,
         text=tr.START_MSG.format(message.from_user.first_name, message.from_user.id),
         parse_mode="markdown",
+ f"""<b>Haii.. 👋🏻 {message.from_user.first_name} Welcome To 𓊈Virtual Music𓊉\n
+Aku Adalah Bot Music Telegram Yang Akan Menemani mu Di Voice Call Group.
+Jika Ingin Menggunakan Invite Aku Dan Asisstantnya Ke Dalam Group Lalu Angkat Bot Menjadi Admin. Jika Ada Kendala Bisa Chat Pemilik Nya.
+┏━━━━━━━━━━━━━━
+┣ > 𝙼𝚎𝚖𝚞𝚝𝚊𝚛 𝚖𝚞𝚜𝚒𝚔 𝚍𝚒𝚐𝚛𝚞𝚙 𝚔𝚊𝚖𝚞.
+┣ > 𝙱𝚒𝚜𝚊 𝚕𝚒𝚜𝚝 𝚕𝚊𝚐𝚞, 𝚌𝚞𝚖𝚊𝚗 𝚓𝚊𝚗𝚐𝚊𝚗 𝚜𝚎𝚔𝚊𝚕𝚒𝚐𝚞𝚜 𝚝𝚊𝚔𝚞𝚝 𝚎𝚛𝚛𝚘𝚛.
+┣ > 𝙼𝚎𝚗𝚌𝚊𝚛𝚒 𝚍𝚊𝚗 𝚖𝚎𝚖𝚞𝚕𝚊𝚒 𝚕𝚊𝚐𝚞 𝚋𝚎𝚛𝚜𝚊𝚖𝚊 𝚝𝚎𝚖𝚊𝚗-𝚝𝚎𝚖𝚊𝚗𝚖𝚞.
+┣ > 𝙼𝚎𝚗𝚌𝚊𝚛𝚒 𝚕𝚊𝚐𝚞 𝚔𝚊𝚖𝚞 𝚍𝚊𝚗 𝚍𝚘𝚠𝚗𝚕𝚘𝚊𝚍 𝚕𝚊𝚐𝚞 𝚔𝚊𝚖𝚞 𝚖𝚎𝚕𝚊𝚕𝚞𝚒 𝚊𝚔𝚞.
+┗━━━━━━━━━━━━━━
+🤵𝓒𝓻𝓮𝓪𝓽𝓮𝓭 𝓫𝔂 : [King](https://t.me/boyfriendnice)
+☘️𝓣𝓱𝓪𝓷𝓴𝓼 𝓯𝓸𝓻 : [Grup Support](https://t.me/Familythunder)
+━━━━━━━━━━━━━━
+𝐁𝐎𝐓 𝐌𝐔𝐒𝐈𝐊 : @Virtualsong_bot - 𝐀𝐒𝐈𝐒𝐒𝐓𝐀𝐍𝐓 𝐌𝐔𝐒𝐈𝐊 : @AsisstantMusicVirtual
+         
         reply_markup=InlineKeyboardMarkup(
             [ 
                 [
                     InlineKeyboardButton(
-                        "🤵Owner Music", url="https://t.me/afterdaytoxic")
+                        "🤵 Owner Music", url="https://t.me/boyfriendnice")
                   ],[
                     InlineKeyboardButton(
-                        "👥Official Group", url="https://t.me/captionanakmuda"
+                        "👥 Official Group", url="https://t.me/Familythunder"
                     ),
                     InlineKeyboardButton(
-                        "📢Official Channel", url="https://t.me/humangabutguys") 
+                        "📢 Official Channel", url="https://t.me/MusikManagement") 
                   ],[
                     InlineKeyboardButton(
-                        "🍀Instagram", url="https://www.instagram.com/ikyyy_35"
+                        "🍀 Instagram", url="https://www.instagram.com/ikyyy_35"
                     )
                 ]
             ]
@@ -81,8 +95,8 @@ def map(pos):
         button = [
             [InlineKeyboardButton("🤵 Owner Music", url="https://t.me/boyfriendnice")
             [InlineKeyboardButton(text = '👥 Support Group', url="https://t.me/Familythunder"
-             InlineKeyboardButton(text = '🍀 Instagram', url="https://www.instagram.com/ikyyy_35"
-            [InlineKeyboardButton(text = '🛠 Source Code 🛠', url=f"https://{SOURCE_CODE}")],
+             InlineKeyboardButton(text = '📢 Support Channel', url="https://t.me/MusikManagement"
+            [InlineKeyboardButton(text = '🍀 Instagram', url="https://www.instagram.com/ikyyy_35"
             [InlineKeyboardButton(text = '◀️Undo', callback_data = f"help+{pos-1}")]
         ]
     else:
